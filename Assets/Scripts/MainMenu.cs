bf534774d3c79d6752b97a2ad86b20618d82937b
Amulya Reddy Konda <amulyareddy.konda@hitachivantara.com>
2021-05-12 11:41:03 +0530

    switch modes

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayVirusMode()
+    {
+        SceneManager.LoadScene(1);
+    }
+
+    public void PlayHumanMode()
+    {
+        SceneManager.LoadScene(2);
+    }
+
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+}
